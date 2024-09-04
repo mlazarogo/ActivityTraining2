a90bc0a1603ea6230f3c2727762093dfc0a79a90
curso <mlazarogo@educa.jcyl.es>
2024-09-04 13:16:40 +0200

    Ejercicio 3 login

--- app/src/main/java/com/iesamml/androidtraining2/features/login/presentation/LoginActivity.kt
+++ app/src/main/java/com/iesamml/androidtraining2/features/login/presentation/LoginActivity.kt
@@ -1,21 +1,53 @@
 package com.iesamml.androidtraining2.features.login.presentation
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.snackbar.Snackbar
 import com.iesamml.androidtraining2.R
+import com.iesamml.androidtraining2.features.login.LoginFactory
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var loginFactory: LoginFactory
+    private lateinit var loginViewModel : LoginViewModel
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+       //enableEdgeToEdge()
+        loginFactory = LoginFactory()
+        loginViewModel = loginFactory.provideLoginViewModel()
         setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        setupView()
+        /*ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
+        }*/
+
+
     }
+    private fun setupView(){
+        val actionValidate = findViewById<Button>(R.id.action_validate)
+        actionValidate.setOnClickListener {
+           val userName = findViewById<EditText>(R.id.input_username).text.toString()
+           val password = findViewById<EditText>(R.id.input_password).text.toString()
+            val isValid=loginViewModel.validateClicked(userName,password)
+            if (isValid) {
+                Snackbar.make (findViewById<View>(R.id.main),
+                                            R.string.message_login_ok,
+                                             Snackbar.LENGTH_SHORT).show()
+            }else {
+                Snackbar.make (findViewById<View>(R.id.main),
+                    R.string.message_login_fails,
+                    Snackbar.LENGTH_SHORT).show()
+            }
+        }
+
+
+    }
+
 }
